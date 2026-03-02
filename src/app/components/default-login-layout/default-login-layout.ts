45714330007e6885b4c68339d7725506be43cb68
ryan <ryan.aguiarh@gmail.com>
2026-03-02 16:40:54 -0300

    feat: style login page and add global CSS variables

--- src/app/components/default-login-layout/default-login-layout.ts
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './default-login-layout.html',
   styleUrl: './default-login-layout.css',
 })
-export class DefaultLoginLayout {}
+export class DefaultLoginLayout {
+  @Input() title:string="";
+  @Input() primaryBtnText: string="";
+  @Input()secundaryBtnText: string="";
+}
